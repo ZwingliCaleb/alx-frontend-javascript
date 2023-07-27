144c3fd599c8b6fd53345e05ea55e6664e4d33fb
ZwingliCaleb <calebzwingli@gmail.com>
2023-07-27 16:42:05 -0700

    implementing a class named holberton school

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+// 2-hbtn_course.js
+
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter and Setter for length
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter and Setter for students
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newStudents;
+  }
+}
+
+export default HolbertonCourse;
+
