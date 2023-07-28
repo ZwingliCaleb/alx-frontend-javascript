611e8f860e68c8cc0291d5a66c9571e9e2fabd60
ZwingliCaleb <calebzwingli@gmail.com>
2023-07-28 15:08:19 -0700

    function to handle response from API

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,10 @@
-// File: 2-then.js
-
-function handleResponseFromAPI(promise) {
-  return promise
-    .then((response) => {
-      console.log("Got a response from the API");
-      return {
-        status: 200,
-        body: "success",
-      };
-    })
-    .catch((error) => {
-      console.log("Got a response from the API");
-      return new Error(); // Return an empty Error object
-    });
+/* eslint-disable no-unreachable */
+export default function handleResponseFromAPI(promise) {
+  return promise.then(
+    () => ({
+      status: 200,
+      body: 'success',
+    }),
+  ).catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
-export default handleResponseFromAPI;
-
