85f8b20dba4fb1b9369774c960b328d30ae2722b
ZwingliCaleb <calebzwingli@gmail.com>
2023-07-28 15:03:29 -0700

    update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,9 @@
-// File: 1-promise.js
-
-function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+export default function getFullResponseFromAPI(success) {
+  if (success) {
+    return Promise.resolve({
+      status: 200,
+      body: 'Success',
+    });
+  }
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
-
-export default getFullResponseFromAPI;
-
