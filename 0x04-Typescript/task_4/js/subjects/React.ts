395e3f7e208b00bbe29f5ef4bbb315e32fdcd3ff
ZwingliCaleb <calebzwingli@gmail.com>
2023-08-03 01:57:57 +0300

    new changes

---
+++ 0x04-Typescript/task_4/js/subjects/React.ts
@@ -0,0 +1,19 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+    export class React extends Subject {
+      getRequirements(): string {
+        return "Here is the list of requirements for React";
+      }
+  
+      getAvailableTeacher(): string {
+        if (this.teacher.experienceTeachingReact !== undefined) {
+          return `Available Teacher: ${this.teacher.firstName}`;
+        } else {
+          return "No available teacher";
+        }
+      }
+    }
+  }
+  
